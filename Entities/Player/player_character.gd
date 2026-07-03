63534186ea0fccdcca3dcb77166e11fd9c5e5553
kyle.ryan <kylejryan67@gmail.com>
2026-07-03 18:56:04 +0200

    tweak: change move input from arrow keys to wasd

--- Entities/Player/player_character.gd
+++ Entities/Player/player_character.gd
@@ -25,7 +25,7 @@ func _physics_process(delta: float) -> void:
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		
 	World.set_time_scale(move_toward(World.get_time_scale(), max(0.02, input_dir.length()), 0.01))
